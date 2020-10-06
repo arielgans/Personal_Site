dc1e15bbbc483aa056038e63f589a59714b2cf40
arielgans <ariel.gans@gmail.com>
2020-10-05 21:32:41 -0400

    Giving up on popovers for now

--- static/script.js
+++ static/script.js
@@ -1,6 +1,19 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+function displayPhone() {
+  var x = document.getElementById("phoneNumber");
+  if (x.style.visibility == "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+} 
+function displayEmail() {
+  var x = document.getElementById("emailAddress");
+  if (x.innerHTML === "") {
+    x.innerHTML = "agans@uwaterloo.ca";
+  } else {
+    x.innerHTML = "";
+  }
+}
 
 function copyPhone() {
   var copyText = "(+1) 416-953-4293";
